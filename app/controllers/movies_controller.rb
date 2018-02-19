a72c74c37dcbfcb93ca18b9ac0a23e9f04b64e1c
Alyssa Lehmann <lehmann.a.m78654@tamu.edu>
2018-02-19 08:45:22 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-
+  # @all_ratings = ['G','PG','PG-13','R']
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
@@ -11,7 +11,31 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = ['G','PG','PG-13','R']
+    # if session[:ratings] == nil
+    #   # @movies = Movie.all
+    #   session[:ratings] = {'G' => 1, 'PG' => 1, 'PG-13' => 1, 'R' => 1}
+    #   params[:ratings] = session[:ratings]
+    # elsif params[:ratings] == nil
+    #   params[:ratings] = session[:ratings]
+    # end
+    
+    if params[:ratings] == nil
+      params[:ratings] = {'G' => 1, 'PG' => 1, 'PG-13' => 1, 'R' => 1}
+      # //session[:ratings] = params[:ratings]
+    end
+    # elsif params[:ratings] == nil
+    #   # params[:ratings] = {'G' => 0, 'PG' => 0, 'PG-13' => 0, 'R' => 0}
+    #   params[:ratings] = session[:ratings]
+    # end
+      
+    # else
+    # params[:ratings] = session[:ratings]
+    @movies = Movie.where(rating: params[:ratings].keys)
+    # session[:ratings] = params[:ratings]
+    # end
+    
+    
   end
 
   def new
@@ -19,6 +43,7 @@ class MoviesController < ApplicationController
   end
 
   def create
+    # @all_ratings = ['G','PG','PG-13','R']
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
@@ -43,17 +68,20 @@ class MoviesController < ApplicationController
   end
   
   def sort_title
+    @all_ratings = ['G','PG','PG-13','R']
     @movies = Movie.order('title')
     params[:title_header] = 'hilite'
     params[:release_date_header] = ''
-    # redirect_to movies_path
+    params[:ratings] = {'G' => 1, 'PG' => 1, 'PG-13' => 1, 'R' => 1}
     render 'index'
   end
   
   def sort_release_date
+    @all_ratings = ['G','PG','PG-13','R']
     @movies = Movie.order('release_date')
     params[:title_header] = ''
     params[:release_date_header] = 'hilite'
+    params[:ratings] = {'G' => 1, 'PG' => 1, 'PG-13' => 1, 'R' => 1}
     render 'index'
   end
 end
