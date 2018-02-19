aa1f106a73d1622ff4098c1681ebd98c1bc528f4
Alyssa Lehmann <lehmann.a.m78654@tamu.edu>
2018-02-19 02:28:55 +0000

    link goes to sort_title html pag

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -41,5 +41,8 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+  
+  def sort_title
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'movies#index'
+  get '/movies/sort_title', to: 'movies#sort_title'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
